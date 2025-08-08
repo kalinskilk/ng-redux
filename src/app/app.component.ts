6ab2c05e25a1827520c831e3135274cc3f48a284
Kalinski <lucascunha@xpert.com.br>
2025-08-08 15:57:48 -0300

    feat: implementacao das ações em app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { TodoFacade } from './facade/todo.facade';
+import { Todo } from './state/todo.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ng-redux';
+  todos$: Observable<Todo[]>;
+  loading$: Observable<boolean>;
+  newTodo = '';
+
+  constructor(private todoFacade: TodoFacade) {
+    this.todos$ = this.todoFacade.todos$;
+    this.loading$ = this.todoFacade.loading$;
+  }
+
+  /**
+   * dispara a action de carregar todas as tarefas => loadTodos()
+   */
+  onLoadTodos() {
+    this.todoFacade.loadTodos();
+  }
+
+  /**
+   * dispara a action de adicionar uma tarefa => addTodo
+   */
+  onAddTodo() {
+    if (this.newTodo.trim()) {
+      this.todoFacade.addTodo(this.newTodo);
+      this.newTodo = '';
+    }
+  }
 }
