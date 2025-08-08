b53b28e622df025cdc7a0e9d550189def26b286d
Kalinski <lucascunha@xpert.com.br>
2025-08-08 14:02:55 -0300

    chore: add @ngrx/store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideEffects()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideEffects(), provideStore()]
 };
